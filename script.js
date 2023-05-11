7ffea0074535a83e7a1fa696a3bf635bdcb73ed4
Ishika Jain <ishika2413@gmail.com>
2023-05-11 06:37:25 +0530

    mclean

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-{/* <button class="follow border-0 mt-4 " onclick="btnclick()">Follow</button> */}
-
-
-  // function btnclick(e){
-  //   console.log("WRF")
-  //   const followbtn = document.querySelectorAll('.follow');
-
-  // for (let i = 0; i < followbtn.length; i++) {
-  //   followbtn[i].addEventListener('click', function() {
-  //     console.log("first")
-  //     followbtn[i].style.backgroundColor = 'black';
-  //   });
-  // }
-  // }
-
   function btnclick(button) {
     console.log("first")
     button.style.backgroundColor = 'black';
